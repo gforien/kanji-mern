826522883e72761ba3c288280a59f9a4948092f4
Gabriel Forien <gforien@gmail.com>
2020-04-22 09:45:18 +0200

    basic backend :white_check_mark:

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-
 /************************************************
  * Express configuration :
  *    - static folder
@@ -21,9 +20,27 @@ app
   .use(express.urlencoded({extended: false}))
   .use((req, res, next) => {
     console.log(req.method + ' ' + req.url);
+    next();
   })
 
 
+
+/**********************************************
+ * Mongoose configuration :
+ *    - ask Mongoose to use ES6 promises
+ *    - connect to database
+ *    - load models
+ **********************************************/
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection
+  .once('open', () => console.log('Connected to database'))
+  .on('error', (error) => console.warn('Error : ',error));
+
+const kanji = mongoose.model('kanji', new Schema({ id: Number, kanji: String}, { collection : 'kanji' }));
 
 
 
@@ -42,11 +59,13 @@ app.get('/', (req, res) => {
   res.status(200).end();
 });
 
-app.get('/:resource', (req, res) => {
-  
-  kanjis = [];
-  // fetch MondoDB resource
-  res.status(200).send(kanjis);
+app.get('/kanjis', (req, res) => {
+  kanji
+  .find()
+  .then((data) => {
+    res.status(200).send(data);
+  })
+  .catch((err) => console.err(err));
 });
 
 
