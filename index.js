088058689263702fb173ee96c820ce7f429a92cb
Gabriel Forien <gforien@gmail.com>
2020-04-22 15:13:48 +0200

    CRUD back-end api :sparkles:

--- index.js
+++ index.js
@@ -15,15 +15,17 @@ const express = require('express');
 const app = express();
 
 app
-//  .use(express.static(__dirname+'/client/public'))
+  //  .use(express.static(__dirname+'/client/public'))
   .use(express.json())
-  .use(express.urlencoded({extended: false}))
+  .use(express.urlencoded({ extended: false }))
   .use((req, res, next) => {
-    console.log(req.method + ' ' + req.url);
+    if (req.body) {
+      console.log(req.method + ' ' + req.url + ' ' + JSON.stringify(req.body));
+    } else {
+      console.log(req.method + ' ' + req.url);
+    }
     next();
-  })
-
-
+  });
 
 /**********************************************
  * Mongoose configuration :
@@ -35,18 +37,19 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 mongoose.connection
   .once('open', () => console.log('Connected to database'))
-  .on('error', (error) => console.warn('Error : ',error));
+  .on('error', (error) => console.warn('Error : ', error));
 
-const kanji = mongoose.model('kanji', new Schema({ id: Number, kanji: String}, { collection : 'kanji' }));
-
-
-
-
-
-
+const kanji = mongoose.model(
+  'kanji',
+  new Schema({ kanji: String }, { collection: 'kanji' })
+);
 
 /****************************************************
  * Routing + REST API
@@ -61,14 +64,52 @@ app.get('/', (req, res) => {
 
 app.get('/kanjis', (req, res) => {
   kanji
-  .find()
-  .then((data) => {
-    res.status(200).send(data);
-  })
-  .catch((err) => console.err(err));
+    .find()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => console.error(err));
 });
 
+app.get('/kanjis/:kanji', (req, res) => {
+  let filter = { kanji: req.params.kanji };
+
+  kanji
+    .findOne(filter)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post('/kanjis', (req, res) => {
+  let newKanji = new kanji(req.body);
+
+  newKanji
+    .save()
+    .then(() => res.status(200).end())
+    .catch((err) => console.error(err));
+});
+
+app.put('/kanjis/:kanji', (req, res) => {
+  let filter = { kanji: req.params.kanji };
+  let update = req.body;
+
+  kanji
+    .findOneAndUpdate(filter, update)
+    .then(() => res.status(200).end())
+    .catch((err) => console.error(err));
+});
+
+app.delete('/kanjis/:kanji', (req, res) => {
+  let filter = { kanji: req.params.kanji };
+
+  kanji
+    .findOneAndDelete(filter)
+    .then(() => res.status(200).end())
+    .catch((err) => console.error(err));
+});
 
 module.exports = app.listen(PORT, () => {
-  console.log('Listening on port '+PORT);
-})
+  console.log('Listening on port ' + PORT);
+});
